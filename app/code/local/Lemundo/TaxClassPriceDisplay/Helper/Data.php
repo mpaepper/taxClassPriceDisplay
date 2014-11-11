e63e1f25026071ffe2d6e34e940b8b406c6734bb
Marc Päpper <m.paepper@lemundo.de>
2014-11-11 11:31:06 +0100

    Adding module basic structure

---
+++ app/code/local/Lemundo/TaxClassPriceDisplay/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+class Lemundo_TaxClassPriceDisplay_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
+	 
---
+++ app/code/local/Lemundo/TaxClassPriceDisplay/Helper/Tax/Data.php
@@ -0,0 +1,5 @@
+<?php
+class Lemundo_TaxClassPriceDisplay_Helper_Tax_Data extends Mage_Tax_Helper_Data
+{
+}
+		
